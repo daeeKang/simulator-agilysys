5d37800a103f821e4c453b54e7443ff6c1693e19
Prabhas Kumra <37914629+prabhaskumra@users.noreply.github.com>
2019-07-18 14:31:45 -0700

    cleaned the file

--- model/RedeemOffer.js
+++ model/RedeemOffer.js
@@ -16,23 +16,14 @@ module.exports = {
         var isSuccess = false
         var errorCode = "", errorMessage = ""
 
-        //var ResponseStatus = {IsSuccess: false, ErrorMessage: "", ErrorCode: ""}
-
-
         var j = 0  // index for the offersAvailable array
+
         while(j < offersAvailable.length){
             for(let i = 0; i < offerData.length; i++){
                 if(offerData[i].AccountNumber === String(accountnumber)){
                     if(offerData[i].OfferCode === offersAvailable[j].OfferCode){
                         redeemOfferResultList.push(offersAvailable[j])
-                        console.log("offer found")
-                        console.log("j== "+j)
-                        console.log("i== "+i)
-                        console.log(offersAvailable.length)
-                        //console.log(redeemOfferResultList[j])
-
                         offerData.splice(i,1)
-                        //offersAvailable.splice(j,1)
                         db.set('offers', offerData).write()
 
                         transactionIdCount++;
@@ -56,9 +47,6 @@ module.exports = {
                 break   
 
             redeemOfferResultList.push(offersAvailable[j])
-            console.log("offer not found")
-            console.log("j== "+j)
-            //console.log(redeemOfferResultList[j])
 
             transactionIdCount++;
             db.set('transactionId', transactionIdCount).write()
@@ -71,21 +59,7 @@ module.exports = {
             }
             redeemOfferResultList[j].ResponseStatus = data
             j++
-            //i = 0
         }
-
-        // if(offersAvailable.length != j){
-        //     responseStatus.IsSuccess = false  // offer found and ready to redeem
-        //     responseStatus.ErrorMessage = "No Offer Found"
-        //     responseStatus.ErrorCode = "#XXXXXX"
-        // }
-        
-        // for(let i = 0; i < redeemOfferResultList.length; i++){
-        //     transactionIdCount++;
-        //     db.set('transactionId', transactionIdCount).write()
-        //     redeemOfferResultList[i].TransactionId = transactionIdCount
-        //     redeemOfferResultList[i].ResponseStatus = responseStatus
-        // }
 
         let out = {
             "AccountNumber": accountnumber,
