a0b50873aeee879888c051c227abe6eb3910f6c0
Prabhas Kumra <37914629+prabhaskumra@users.noreply.github.com>
2019-07-17 17:00:10 -0700

    something is wrong.
    Just saving it.

--- model/RedeemOffer.js
+++ model/RedeemOffer.js
@@ -13,33 +13,88 @@ module.exports = {
         let redeemOfferResultList = []
         let transactionIdCount = db.get('transactionId').value()
 
-        var j = 0
-        for(let i = 0; i < offerData.length; i++){
-            if(offerData[i].AccountNumber === String(accountnumber)){
-                if(offerData[i].OfferCode === offersAvailable[j].OfferCode){
-                    redeemOfferResultList.push(offersAvailable[j])
-                    var removed = offerData.splice(i,1)
-                    console.log(removed)
-                    db.set('offers', offerData).write()
-                    j++
-                    if(offersAvailable.length === j)
-                        break
-                }
-            }
-        }
-        var responseStatus = {"IsSuccess": true, "ErrorMessage": "", "ErrorCode": ""}
+        var isSuccess = false
+        var errorCode = "", errorMessage = ""
 
-        for(let i = 0; i < redeemOfferResultList.length; i++){
+        //var ResponseStatus = {IsSuccess: false, ErrorMessage: "", ErrorCode: ""}
+
+
+        var j = 0
+
+        while(j < offersAvailable.length){
+
+            for(let i = 0; i < offerData.length; i++){
+                if(offerData[i].AccountNumber === String(accountnumber)){
+
+                    if(offerData[i].OfferCode === offersAvailable[j].OfferCode){
+                        redeemOfferResultList.push(offersAvailable[j])
+                        console.log("i am not here")
+                        console.log(redeemOfferResultList[j])
+
+                        offerData.splice(i,1)
+                        //offersAvailable.splice(j,1)
+
+                        //console.log(removed)
+                        db.set('offers', offerData).write()
+
+                        redeemOfferResultList[j].ResponseStatus.IsSuccess = true  // offer found and ready to redeem
+                        redeemOfferResultList[j].ResponseStatus.ErrorMessage = ""
+                        redeemOfferResultList[j].ResponseStatus.ErrorCode = ""
+
+                        transactionIdCount++;
+                        db.set('transactionId', transactionIdCount).write()
+                        redeemOfferResultList[j].TransactionId = transactionIdCount
+                        //redeemOfferResultList[j].ResponseStatus = responseStatus
+
+                        j++
+                        i = 0
+                    }
+                }
+                if(offersAvailable.length === j)
+                    break
+            }
+
+            if(offersAvailable.length === j)
+                break   
+
+            redeemOfferResultList.push(offersAvailable[j])
+            console.log("i am here")
+            console.log(redeemOfferResultList[j])
+
+            redeemOfferResultList[j].ResponseStatus.IsSuccess = false  // offer found and ready to redeem
+            redeemOfferResultList[j].ResponseStatus.ErrorMessage = "Offer Not Found"
+            redeemOfferResultList[j].ResponseStatus.ErrorCode = "XXXX"
+
             transactionIdCount++;
             db.set('transactionId', transactionIdCount).write()
-            redeemOfferResultList[i].TransactionId = transactionIdCount
-            redeemOfferResultList[i].ResponseStatus = responseStatus
+            redeemOfferResultList[j].TransactionId = transactionIdCount
+            redeemOfferResultList[j].ResponseStatus = responseStatus
+
+            j++
+
         }
+
+        // if(offersAvailable.length != j){
+        //     responseStatus.IsSuccess = false  // offer found and ready to redeem
+        //     responseStatus.ErrorMessage = "No Offer Found"
+        //     responseStatus.ErrorCode = "#XXXXXX"
+        // }
+        
+        // for(let i = 0; i < redeemOfferResultList.length; i++){
+        //     transactionIdCount++;
+        //     db.set('transactionId', transactionIdCount).write()
+        //     redeemOfferResultList[i].TransactionId = transactionIdCount
+        //     redeemOfferResultList[i].ResponseStatus = responseStatus
+        // }
 
         let out = {
             "AccountNumber": accountnumber,
             RedeemOfferResultList: redeemOfferResultList,
-            "ResponseStatus": responseStatus,
+            "ResponseStatus": {
+                "IsSuccess": true,
+                "ErrorMessage": "",
+                "ErrorCode": ""
+              },
               "CustomFields": {}
         }
 
