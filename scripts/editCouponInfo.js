1d741283bb9d4533f4b17dde3d059f7743861b0d
Prabhas Kumra <37914629+prabhaskumra@users.noreply.github.com>
2019-07-19 14:08:50 -0700

    organised coupon data into the table

--- scripts/editCouponInfo.js
+++ scripts/editCouponInfo.js
@@ -2,6 +2,27 @@ var couponData = db.get('coupons').value()
 
 
 function updateCouponTable(){
+    var couponData = db.get('coupons').value()
+    console.log(couponData)
+    let accountInfo = ""
+    accountInfo +=(
+        "<table id='couponTable' border='1' width='700'>" + 
+        "<tr><th>Coupon Number</th><th>Redeem Amount</th></tr>"
+    )
+
+    for(let i = 0; i < couponData.length; i++){
+        accountInfo+=(
+            "<tr><td> <input type='text' class='input input1' value ='" +couponData[i].CouponNumber +"'onchange='swapValue(this,"+i+")'"+"</td>" +
+            "<td> <input type='text' name='redeemAccount' class='input input1' value='"+couponData[i].RedeemAmount +"'onchange='swapValue(this,"+i+")'"+"</td></tr>"
+        )
+        
+    }
+
+    accountInfo += "</table>"
+    document.getElementById('editCoupon').innerHTML = accountInfo
+}
+
+function swapValue(tableElement, i){
 
 }
 
@@ -14,7 +35,7 @@ function editCouponData(){
   }
 
 
-function loadMain(){
+function loadCouponMain(){
     updateCouponTable()
     document.getElementById('edit-coupons').style.display = 'none'
     document.getElementById('main-page').style.display = 'block'
