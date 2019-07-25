e00c70532784a87d496cd836e1a1dd75e0d742db
Prabhas Kumra <37914629+prabhaskumra@users.noreply.github.com>
2019-07-25 09:45:42 -0700

    added all the data into the table

--- scripts/editOfferData.js
+++ scripts/editOfferData.js
@@ -1,10 +1,28 @@
 var offersData = db.get('offers').value()
+var playerData = db.get('players').value()
 
 
 function updateOfferTable(){
     db.read()
     offersData = db.get('offers').value()
-    
+
+    let offersInfo = ""
+    offersInfo +=(
+        "<table id='offerTable' border='1' style='width: 500px'>" + 
+        "<tr><th>Player Name</th><th>Account Numvber</th> <th>Offers Available</th></tr>"
+    )
+
+    for(let i = 0; i < playerData.length; i++){
+        offersInfo +=(
+            "<tr><td>"+playerData[i].firstName +" "+playerData[i].lastName +"</td>" +
+            "<td>"+playerData[i].accountNumber+"</td>"+
+            "<td><button class='button button2' id='edit-playerOffers'>View Offers</button></td></tr>"
+        )
+    }
+
+    offersInfo += "</table>"
+    document.getElementById('editOffer').innerHTML = offersInfo
+
 }
 
 function searchOfferTable(){
@@ -17,6 +35,7 @@ function writeOfferData(){
 
 
 function editOfferData(){
+    updateOfferTable()
     document.getElementById('edit-offers').style.display = 'block'
     document.getElementById('main-page').style.display = 'none'
     document.getElementById('settings-button').style.visibility = 'hidden'
@@ -24,6 +43,7 @@ function editOfferData(){
 }
 
 function loadOfferMain(){
+    updateOfferTable()
     document.getElementById('edit-offers').style.display = 'none'
     document.getElementById('main-page').style.display = 'block'
     document.getElementById('settings-button').style.visibility = 'visible'
