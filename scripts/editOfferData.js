480163f1d3ff34318b9745097c0d1582bb825f71
Prabhas Kumra <37914629+prabhaskumra@users.noreply.github.com>
2019-07-25 14:52:21 -0700

    some changes

--- scripts/editOfferData.js
+++ scripts/editOfferData.js
@@ -16,7 +16,9 @@ function updateOfferTable(){
         offersInfo +=(
             "<tr><td>"+playerData[i].firstName +" "+playerData[i].lastName +"</td>" +
             "<td>"+playerData[i].accountNumber+"</td>"+
-            "<td><button class='button button2' id='edit-playerOffers'>View Offers</button></td></tr>"
+            "<td><button class='button button2' id='edit-playerOffers' onclick='viewPlayerOffers("+i+")' >View Offers</button></td></tr>"
+            // "<td><label for='offers'>View Offers</label> <input type='checkbox' name='offersToggle' id='offersToggle' data-toggle='toggle'></td> </tr>"+
+            // "<tbody class='hide'> <tr> <td>"+offersData[i]+"</td></tr></tbody>"
         )
     }
 
@@ -25,12 +27,46 @@ function updateOfferTable(){
 
 }
 
-function searchOfferTable(){
+function viewPlayerOffers(i){
+    playerOffersTable()
+    
 
+}
+
+function searchOfferTable(input){
+    //filterOfferTable(input.value)
+}
+
+function filterOfferTable(value){
+
+    let offersInfo = ""
+    offersInfo +=(
+        "<table id='offerTable' border='1' style='width: 500px'>" + 
+        "<tr><th>Player Name</th><th>Account Numvber</th> <th>Offers Available</th></tr>"
+    )
+
+    for(let i = 0; i < playerData.length; i++){
+        if(playerData[i].accountNumber.includes(value) || playerData[i].firstName.toLowerCase().includes(value.toLowerCase()) || playerData[i].lastName.toLowerCase().includes(value.toLowerCase())){
+            offersInfo +=(
+                "<tr><td>"+playerData[i].firstName +" "+playerData[i].lastName +"</td>" +
+                "<td>"+playerData[i].accountNumber+"</td>"+
+                "<td><button class='button button2' id='edit-playerOffers' onclick='viewPlayerOffers("+i+")' >View Offers</button></td></tr>"
+            )
+        }    
+    }
+
+    offersInfo += "</table>"
+    document.getElementById('editOffer').innerHTML = offersInfo
 }
 
 function writeOfferData(){
 
+}
+
+function playerOffersTable(){
+    document.getElementById('edit-offers').style.display = 'block'
+    document.getElementById('main-page').style.display = 'none'
+    document.getElementById('settings-button').style.visibility = 'hidden'
 }
 
 
@@ -48,3 +84,9 @@ function loadOfferMain(){
     document.getElementById('main-page').style.display = 'block'
     document.getElementById('settings-button').style.visibility = 'visible'
 }
+
+// $(document).ready(function() {
+// 	$('[data-toggle="toggle"]').change(function(){
+// 		$(this).parents().next('.hide').toggle();
+// 	});
+// });
