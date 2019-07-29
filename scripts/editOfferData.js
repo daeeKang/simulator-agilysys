2822aa088f633212c701e2ad71d7cf7f45a1d687
daee kang <44410043+daeeKang@users.noreply.github.com>
2019-07-29 11:04:00 -0700

    lol

--- scripts/editOfferData.js
+++ scripts/editOfferData.js
@@ -7,93 +7,81 @@ let offersInfo = ""
 function updateOfferTable(){
     db.read()
     offersData = db.get('offers').value()
-
-    // let offersInfo = ""
+    
+    offersInfo = ""
     offersInfo +=(
-        "<table id='offerTable' border='1' style='width: 500px'>" + 
-        "<tr><th>Player Name</th><th>Account Number</th> <th>Offers Available</th></tr>"
+        "<table id='offerTable' border='1' style='width: 700px'>" + 
+        "<tr><th>Account Number</th><th>Offer Name</th><th>Offer #</th><th>Offer Amount</th></tr>"
     )
 
-    for(let i = 0; i < playerData.length; i++){
+    for(let i = 0; i < offersData.length; i++){
+
         offersInfo +=(
-
-            "<table id='offerTable"+i+"' border='1' style='width: 500px'>" + 
-        //    "<tr><th>Player Name</th><th>Account Number</th> <th>Offers Available</th></tr>"+
-    
-
-            "<tr><td>"+playerData[i].firstName +" "+playerData[i].lastName +"</td>" +
-            "<td>"+playerData[i].accountNumber+"</td>"+
-<<<<<<< Updated upstream
-            "<td><button class='button button2' id='edit-playerOffers' data-toggle='modal' data-target='.bd-example-modal-lg' onclick='viewPlayerOffers("+i+")' >View Offers</button></td></tr>"
-            // "<td><label for='offers'>View Offers</label> <input type='checkbox' name='offersToggle' id='offersToggle' data-toggle='toggle'></td> </tr>"+
-            // "<tbody class='hide'> <tr> <td>"+offersData[i]+"</td></tr></tbody>"
-=======
-            "<td><button class='button button2' data-toggle='collapse' id='edit-playerOffers' data-target='#collapseExample"+i+"' aria-expanded='false' aria-controls='collapseExample' onclick='viewPlayerOffers("+i+")' >View Offers</button></td></tr>"+
-            //"<td><label for='offers'>View Offers</label> <input type='checkbox' name='offersToggle' id='offersToggle' data-toggle='toggle'></td> </tr>"+
-            //"<tbody class='hide'> <tr> <td>"+offersData[i]+"</td></tr></tbody>"
-            "</table>"+
-
-            "<div class='collapse' id='collapseExample"+i+"' >"+
-                "<div class='card card-body'>"+
-                "<td><input type='text2' border='1' name='offerCode' value='Hey'></input></td>"+
-            "</div> </div>"
-
->>>>>>> Stashed changes
-        )
-    }
-
-     offersInfo += "</table>"
-    document.getElementById('editOffer').innerHTML = offersInfo
-
-}
-
-function viewPlayerOffers(i){
-    
-    // playerOffersTable()
-    for(var j = 0; j < offersData.length; j++){
-        if(playerData[i].accountNumber === offersData[j].AccountNumber){
-            console.log("here")
-            offersInfo+=(
-                "<tr id='collapse"+i+"' class='panel-collapse collapse"+i+"' >"+
-                "<td ><input type='text' value='"+offersData[j].offerCode+"'></input></td>"+
-                "<td ><input type='text' value='"+offersData[j].offerName+"'></input></td>"+
-                "<td ><input type='text' value='"+offersData[j].offerValue+"'></input></td></tr>"
+            "<tr><td><input type='text' name='accountName' class='input input1' value='" + offersData[i].AccountNumber + "' onchange='swapOffer(this,"+i+")'></td>" + 
+            "<td><input type='text' name='offerName' class='input input1' value='" + offersData[i].OfferName + "' onchange='swapOffer(this,"+i+")'></td>" + 
+            "<td><input type='text' name='offerCode' class='input input1' value='" + offersData[i].OfferCode + "' onchange='swapOffer(this,"+i+")'></td>" + 
+            "<td><input type='text' name='offerValue' class='input input1' value='" + offersData[i].OfferValue + "' onchange='swapOffer(this,"+i+")'></td></tr>"        
             )
-            document.getElementById('editOffer').innerHTML = offersInfo
-        }
     }
-    
+    offersInfo += "</table>"
 
+    document.getElementById('editOffer').innerHTML = offersInfo
 }
 
-function searchOfferTable(input){
-    //filterOfferTable(input.value)
-}
+function searchOfferTable(input) {
+    filterOfferTable(input.value)
+  }
 
 function filterOfferTable(value){
-
-    let offersInfo = ""
+    offersData = db.get('offers').value()
+    offersInfo = ""
     offersInfo +=(
-        "<table id='offerTable' border='1' style='width: 500px'>" + 
-        "<tr><th>Player Name</th><th>Account Numvber</th> <th>Offers Available</th></tr>"
+        "<table id='offerTable' border='1' style='width: 700px'>" + 
+        "<tr><th>Account Number</th><th>Offer Name</th><th>Offer #</th><th>Offer Amount</th></tr>"
     )
 
-    for(let i = 0; i < playerData.length; i++){
-        if(playerData[i].accountNumber.includes(value) || playerData[i].firstName.toLowerCase().includes(value.toLowerCase()) || playerData[i].lastName.toLowerCase().includes(value.toLowerCase())){
+    for(let i = 0; i < offersData.length; i++){
+        if(String(offersData[i].AccountNumber).includes(value))
             offersInfo +=(
-                "<tr><td>"+playerData[i].firstName +" "+playerData[i].lastName +"</td>" +
-                "<td>"+playerData[i].accountNumber+"</td>"+
-                "<td><button class='button button2' id='edit-playerOffers' onclick='viewPlayerOffers("+i+")' >View Offers</button></td></tr>"
-            )
-        }    
+                "<tr><td><input type='text' name='accountName' class='input input1' value='" + offersData[i].AccountNumber + "' onchange='swapOffer(this,"+i+")'></td>" + 
+                "<td><input type='text' name='offerName' class='input input1' value='" + offersData[i].OfferName + "' onchange='swapOffer(this,"+i+")'></td>" + 
+                "<td><input type='text' name='offerCode' class='input input1' value='" + offersData[i].OfferCode + "' onchange='swapOffer(this,"+i+")'></td>" + 
+                "<td><input type='text' name='offerValue' class='input input1' value='" + offersData[i].OfferValue + "' onchange='swapOffer(this,"+i+")'></td></tr>"        
+                )
     }
-
     offersInfo += "</table>"
+
     document.getElementById('editOffer').innerHTML = offersInfo
 }
 
-function writeOfferData(){
+function swapOffer(tableElement, i) {
+    switch(tableElement.name){
+      case 'accountName': {
+        offersData[i].AccountNumber = tableElement.value
+        break
+      }
+      case 'offerName': {
+        offersData[i].OfferName = tableElement.value
+        break
+      }
+      case 'offerCode': {
+        offersData[i].OfferCode = tableElement.value
+        break
+      }
+      case 'offerValue': {
+        offersData[i].OfferValue = tableElement.value
+        break
+      }
+    }
+  }
 
+function saveOffers(){
+    db.set('offers', offersData).write()
+
+    document.getElementById('offerDataSaved').textContent = "Data Saved Successfully!"
+    setTimeout(function(){
+        document.getElementById('offerDataSaved').textContent = "";
+    },1000);
 }
 
 function playerOffersTable(){
@@ -117,23 +105,3 @@ function loadOfferMain(){
     document.getElementById('main-page').style.display = 'block'
     document.getElementById('settings-button').style.visibility = 'visible'
 }
-
-// $(document).ready(function() {
-// 	$('[data-toggle="toggle"]').change(function(){
-// 		$(this).parents().next('.hide').toggle();
-// 	});
-// });
-<<<<<<< Updated upstream
-=======
-
-
-// function mergeData(){
-
-//     for(var j = 0; j < playerData.length; j++){
-//       if(playerData[i].accountNumber === offersData[i].accountNumber){
-//         var merge= playerData[i]
-        
-//       }
-//     }
-//   }
->>>>>>> Stashed changes
