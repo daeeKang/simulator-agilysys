212364a5e1a6d660bdc96f4f4fdbc8562a1512b8
Prabhas Kumra <37914629+prabhaskumra@users.noreply.github.com>
2019-07-29 10:07:21 -0700

    all changes

--- scripts/editOfferData.js
+++ scripts/editOfferData.js
@@ -1,22 +1,22 @@
 var offersData = db.get('offers').value()
 var playerData = db.get('players').value()
-
+let offersInfo = ""
 
 function updateOfferTable(){
     db.read()
     offersData = db.get('offers').value()
 
-    let offersInfo = ""
+    // let offersInfo = ""
     offersInfo +=(
         "<table id='offerTable' border='1' style='width: 500px'>" + 
-        "<tr><th>Player Name</th><th>Account Numvber</th> <th>Offers Available</th></tr>"
+        "<tr><th>Player Name</th><th>Account Number</th> <th>Offers Available</th></tr>"
     )
 
     for(let i = 0; i < playerData.length; i++){
         offersInfo +=(
             "<tr><td>"+playerData[i].firstName +" "+playerData[i].lastName +"</td>" +
             "<td>"+playerData[i].accountNumber+"</td>"+
-            "<td><button class='button button2' id='edit-playerOffers' onclick='viewPlayerOffers("+i+")' >View Offers</button></td></tr>"
+            "<td><button class='button button2' id='edit-playerOffers' data-toggle='modal' data-target='.bd-example-modal-lg' onclick='viewPlayerOffers("+i+")' >View Offers</button></td></tr>"
             // "<td><label for='offers'>View Offers</label> <input type='checkbox' name='offersToggle' id='offersToggle' data-toggle='toggle'></td> </tr>"+
             // "<tbody class='hide'> <tr> <td>"+offersData[i]+"</td></tr></tbody>"
         )
@@ -28,7 +28,20 @@ function updateOfferTable(){
 }
 
 function viewPlayerOffers(i){
-    playerOffersTable()
+    
+    // playerOffersTable()
+    for(var j = 0; j < offersData.length; j++){
+        if(playerData[i].accountNumber === offersData[j].AccountNumber){
+            console.log("here")
+            offersInfo+=(
+                "<tr id='collapse"+i+"' class='panel-collapse collapse"+i+"' >"+
+                "<td ><input type='text' value='"+offersData[j].offerCode+"'></input></td>"+
+                "<td ><input type='text' value='"+offersData[j].offerName+"'></input></td>"+
+                "<td ><input type='text' value='"+offersData[j].offerValue+"'></input></td></tr>"
+            )
+            document.getElementById('editOffer').innerHTML = offersInfo
+        }
+    }
     
 
 }
@@ -79,7 +92,7 @@ function editOfferData(){
 }
 
 function loadOfferMain(){
-    updateOfferTable()
+    //updateOfferTable()
     document.getElementById('edit-offers').style.display = 'none'
     document.getElementById('main-page').style.display = 'block'
     document.getElementById('settings-button').style.visibility = 'visible'
