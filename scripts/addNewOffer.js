cdc9e00bbdb152935c7a5641d32a6a3cd4bd8f24
Prabhas Kumra <37914629+prabhaskumra@users.noreply.github.com>
2019-07-29 14:07:24 -0700

    added new window to add new offer

---
+++ scripts/addNewOffer.js
@@ -0,0 +1,16 @@
+
+function loadAddOffer(){
+    document.getElementById('add-offer').style.display = 'block'
+    document.getElementById('edit-offers').style.display = 'none'
+}
+
+function loadAddOfferMain(){
+    document.getElementById('add-offer').style.display = 'none'
+    document.getElementById('edit-offers').style.display = 'block'
+
+}
+
+function saveNewOffer(){
+
+}
+
